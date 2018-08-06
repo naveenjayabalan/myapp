d047911077725e5df5d4ff3e8bdf0b7ca73c7202
naveensaravanan <jayabalannaveen@gmail.com>
2018-08-06 17:58:42 +0530

    Changed by naveen

--- src/app/navigation/menu.component.ts
+++ src/app/navigation/menu.component.ts
@@ -15,6 +15,24 @@ export class MenuComponent implements OnInit {
       $('.period-selection').toggleClass('show-period');
       $('.popover-child').css('display', 'none');
   });
+
+  $(function () {
+    $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+    $('.tree li ul > li').hide();
+      $('.tree li.parent_li > span').on('click', function (e) {
+        const children = $(this).parent('li.parent_li').find(' > ul > li');
+        if (children.is(':visible')) {
+            children.hide('fast');
+            $(this).attr('title', 'Expand this branch').find(' > i').addClass('fa-chevron-right').removeClass('fa-chevron-down');
+
+        } else {
+            children.show('fast');
+            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('fa-chevron-down').removeClass('fa-chevron-right');
+               // $(".tree-menu-list ul").toggle("collapse");
+        }
+        e.stopPropagation();
+      });
+      });
   }
 
 }
